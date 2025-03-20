02f80f1ae493f84441260d04e1fb55ab14d54e2a
Suryanshu <csuryanshu.koch@gmail.com>
2025-03-20 16:17:07 +0530

    Update arch-post-install.sh

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -2,9 +2,58 @@
 
 echo "Starting Post-Install"
 echo "Updating system"
+
 sudo pacman -Syu
 
-echo "Installing essential packages$"
-sudo pacman -S p7zip nwg-look gvfs-mtp cliphist waylock pavucontrol wlsunset tllist zsh git htop curl wget neovim neofetch dunst brightnessctl wob ttf-hack ttf-jetbrains-mono ttf-roboto ttf-dejavu noto-fonts noto-fonts-extra noto-fonts-cjk wlroots wayland-protocols pixman tllist fcft foot lf chafa libsixel thunar file-roller thunar-archive-plugin swww feh mpv zathura zathura-pdf-poppler bluez bluez-utils xorg-xwayland ufw
+echo "Installing essential packages"
+sudo pacman -S --noconfirm p7zip nwg-look man gvfs-mtp cliphist waylock wlsunset tllist zsh git htop curl wget neovim dunst neofetch pavucontrol brightnessctl wob ttf-hack ttf-jetbrains-mono ttf-roboto ttf-dejavu ttf-nerd-fonts-symbols noto-fonts noto-fonts-extra noto-fonts-cjk wlroots wayland-protocols pixman fcft foot lf chafa libsixel thunar file-roller thunar-archive-plugin swww gimp obsidian keepassxc libreoffice-fresh feh mpv zathura zathura-pdf-poppler papirus-icon-theme bluez bluez-utils xorg-xwayland
 
-echo "Post-instal setup complete!"
+echo "Installing yay (AUR helper)"
+mkdir ~/yay
+git clone https://aur.archlinux.org/yay-bin.git ~/yay 
+cd ~/yay
+makepkg -si
+cd 
+rm -rf yay
+
+sudo pacman -Rns yay-bin-debug
+
+echo "Installing AUR packages"
+yay -Sy --noconfirm --needed brave-bin
+
+echo "Changing default shell to zsh"
+chsh -s $(which zsh)
+
+mkdir ~/.temp
+mkdir ~/.temp/.config
+mkdir ~/.temp/SucklessWayland
+mkdir ~/.temp/.scripts
+
+git clone https://github.com/realsuryanshu/.config.git ~/.temp/.config && git clone https://github.com/realsuryanshu/SucklessWayland.git ~/.temp/SucklessWayland && git clone https://github.com/realsuryanshu/.scripts.git ~/.temp/.scripts
+
+mkdir ~/.cfg
+mkdir ~/.etc
+mkdir ~/.etc/suckless
+
+mv ~/.temp/.config/* ~/.cfg/
+mv ~/.temp/SucklessWayland/* ~/.etc/suckless/
+mv ~/.temp/.scripts ~/
+
+sudo ln -s ~/.scripts/* /usr/local/bin
+sudo rm -rf /usr/local/bin/statusbar
+sudo rm /usr/local/bin/arch-post-install.sh
+
+ln -s ~/.cfg/dunst ~/.config/
+ln -s ~/.cfg/foot ~/.config/
+ln -s ~/.cfg/lf ~/.config/
+ln -s ~/.cfg/wob ~/.config/
+ln -s ~/.cfg/zsh/.* ~/
+
+cd ~/.etc/suckless/dwl
+sudo make clean install
+cd ~/.etc/suckless/mew
+sudo make clean install
+
+rm -rf ~/.temp
+
+echo "Post-install setup complete!"
